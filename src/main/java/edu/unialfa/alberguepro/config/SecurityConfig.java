06a63b2da38e846968fbd71b6eee7ff489ca4583
Gabriel Paiva de Almeida <gabriel.almeida@pmu.localdomain>
2025-09-03 08:18:49 -0300

    Implementação do AdminLTE no Projeto

---
+++ src/main/java/edu/unialfa/alberguepro/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package edu.unialfa.alberguepro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/webjars/**", "/login").permitAll() // Permite acesso à página de login e aos recursos do AdminLTE
+                        .anyRequest().authenticated() // Exige autenticação para todas as outras páginas
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login") // Define a sua página de login personalizada
+                        .defaultSuccessUrl("/", true) // Redireciona para a página inicial após o login
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Cria um usuário em memória para teste
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
